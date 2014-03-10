fadf69d491b261fcaa2179cabdc93ec5cc04aa9c
exe44 <exelion@gmail.com>
2014-03-10 14:01:31 +0800

    add Color::GRAY

--- src/math_helper.cpp
+++ src/math_helper.cpp
@@ -1691,6 +1691,7 @@ namespace ERI {
 
 	const Color Color::WHITE;
 	const Color Color::BLACK = Color(0.f, 0.f, 0.f);
+	const Color Color::GRAY = Color(0.5f, 0.5f, 0.5f);
 	const Color Color::RED = Color(1.f, 0.f, 0.f);
 	const Color Color::GREEN = Color(0.f, 1.f, 0.f);
 	const Color Color::BLUE = Color(0.f, 0.f, 1.f);
